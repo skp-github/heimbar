ed9c5ad7208c182b804cc20b609d9374158120a5
vgg <niceglassnz@gmail.com>
2021-09-18 20:01:39 +0200

    analytics quick change for sales

--- web_views/urls.py
+++ web_views/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from web_views.views import SpecialsListView, SalesOverviewListView
 
-from web_views.views import SpecialsListView
 urlpatterns = [
     path('specials/', SpecialsListView.as_view(), name="specials_list"),
+    path('drinks_analytics/', SalesOverviewListView.as_view(), name="drinks_analytics"),
 ]
--- web_views/views.py
+++ web_views/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from inventory.models import Cocktail, Mocktail
+from inventory.models import Cocktail, Mocktail, Drink
+from orders.models import Order, OrderItem
 from datetime import date
 
 # Create your views here.
@@ -17,3 +18,28 @@ class SpecialsListView(ListView):
     #     ctx = super().get_context_data(object_list=object_list, **kwargs)
     #     ctx['count'] = get_panel_count(self.request.user)
     #     return ctx
+
+class SalesOverviewListView(ListView):
+    template_name = 'web/heimbar/sales_analytics.html'
+
+    def get_queryset(self):
+        qs = {}
+        orders = Order.objects.all()
+        total_sales = 0
+        qs['number_of_orders'] = len(orders)
+        for order_loop in orders:
+            total_sales += order_loop.total_price
+        qs['total_sales'] = total_sales
+        drinks = Drink.objects.all()
+        cocktails = Cocktail.objects.all()
+        mocktails = Mocktail.objects.all()
+        drink_count = {}
+        # for drink in drinks:
+        #     drink_count[drink.drink_name] = OrderItem.objects.filter(drink=drink).count()
+        # for cocktail in cocktails:
+        #     drink_count[cocktail.name] = OrderItem.objects.filter(cocktail=cocktail).count()
+        # for mocktail in mocktails:
+        #     drink_count[mocktail.name] = OrderItem.objects.filter(mocktail=mocktail).count()
+        # qs['drinks_sale_count'] =  drink_count
+
+        return qs
